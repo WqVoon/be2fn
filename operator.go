47f2ddc4e0064caa48295786412005dad8a8f660
WqVoon <2027195958@qq.com>
2023-02-04 00:37:04 +0800

    feat: OperatorSetIface 增加字符串的大小比较函数和布尔值的判等函数

--- operator.go
+++ operator.go
@@ -8,30 +8,42 @@ type OperatorSetIface interface {
 	// >
 	GreaterThenIntLeft(varname string, val int64) Unit
 	GreaterThenIntRight(val int64, varname string) Unit
+	GreaterThenStringLeft(varname string, val string) Unit
+	GreaterThenStringRight(val string, varname string) Unit
 
 	// >=
 	GreaterThenOrEqualToIntLeft(varname string, val int64) Unit
 	GreaterThenOrEqualToIntRight(val int64, varname string) Unit
+	GreaterThenOrEqualToStringLeft(varname string, val string) Unit
+	GreaterThenOrEqualToStringRight(val string, varname string) Unit
 
 	// <
 	LessThenIntLeft(varname string, val int64) Unit
 	LessThenIntRight(val int64, varname string) Unit
+	LessThenStringLeft(varname string, val string) Unit
+	LessThenStringRight(val string, varname string) Unit
 
 	// <=
 	LessThenOrEqualToIntLeft(varname string, val int64) Unit
 	LessThenOrEqualToIntRight(val int64, varname string) Unit
+	LessThenOrEqualToStringLeft(varname string, val string) Unit
+	LessThenOrEqualToStringRight(val string, varname string) Unit
 
 	// ==
 	EqualToIntLeft(varname string, val int64) Unit
 	EqualToIntRight(val int64, varname string) Unit
 	EqualToStringLeft(varname string, val string) Unit
 	EqualToStringRight(val string, varname string) Unit
+	EqualToBoolLeft(varname string, val bool) Unit
+	EqualToBoolRight(bool string, varname string) Unit
 
 	// !=
 	NotEqualToIntLeft(varname string, val int64) Unit
 	NotEqualToIntRight(val int64, varname string) Unit
 	NotEqualToStringLeft(varname string, val string) Unit
 	NotEqualToStringRight(val string, varname string) Unit
+	NotEqualToBoolLeft(varname string, val bool) Unit
+	NotEqualToBoolRight(bool string, varname string) Unit
 }
 
 // &&
@@ -66,11 +78,23 @@ func (*DefaultOperatorSet) GreaterThenIntLeft(varname string, val int64) Unit {
 func (*DefaultOperatorSet) GreaterThenIntRight(val int64, varname string) Unit {
 	return nil
 }
+func (*DefaultOperatorSet) GreaterThenStringLeft(varname string, val string) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) GreaterThenStringRight(val string, varname string) Unit {
+	return nil
+}
 
 func (*DefaultOperatorSet) GreaterThenOrEqualToIntLeft(varname string, val int64) Unit {
 	return nil
 }
 func (*DefaultOperatorSet) GreaterThenOrEqualToIntRight(val int64, varname string) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) GreaterThenOrEqualToStringLeft(varname string, val string) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) GreaterThenOrEqualToStringRight(val string, varname string) Unit {
 	return nil
 }
 
@@ -80,11 +104,23 @@ func (*DefaultOperatorSet) LessThenIntLeft(varname string, val int64) Unit {
 func (*DefaultOperatorSet) LessThenIntRight(val int64, varname string) Unit {
 	return nil
 }
+func (*DefaultOperatorSet) LessThenStringLeft(varname string, val string) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) LessThenStringRight(val string, varname string) Unit {
+	return nil
+}
 
 func (*DefaultOperatorSet) LessThenOrEqualToIntLeft(varname string, val int64) Unit {
 	return nil
 }
 func (*DefaultOperatorSet) LessThenOrEqualToIntRight(val int64, varname string) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) LessThenOrEqualToStringLeft(varname string, val string) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) LessThenOrEqualToStringRight(val string, varname string) Unit {
 	return nil
 }
 
@@ -100,6 +136,12 @@ func (*DefaultOperatorSet) EqualToStringLeft(varname string, val string) Unit {
 func (*DefaultOperatorSet) EqualToStringRight(val string, varname string) Unit {
 	return nil
 }
+func (*DefaultOperatorSet) EqualToBoolLeft(varname string, val bool) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) EqualToBoolRight(bool string, varname string) Unit {
+	return nil
+}
 
 func (*DefaultOperatorSet) NotEqualToIntLeft(varname string, val int64) Unit {
 	return nil
@@ -111,5 +153,11 @@ func (*DefaultOperatorSet) NotEqualToStringLeft(varname string, val string) Unit
 	return nil
 }
 func (*DefaultOperatorSet) NotEqualToStringRight(val string, varname string) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) NotEqualToBoolLeft(varname string, val bool) Unit {
+	return nil
+}
+func (*DefaultOperatorSet) NotEqualToBoolRight(bool string, varname string) Unit {
 	return nil
 }
