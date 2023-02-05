e4f4bcbdfa1d6973cf3e28afa2353d56f50d7dc9
WqVoon <2027195958@qq.com>
2023-02-05 10:51:01 +0800

    feat: Kv.GetXXX 系方法返回 error

--- kv.go
+++ kv.go
@@ -1,12 +1,19 @@
 package be2fn
 
+import (
+	"fmt"
+)
+
 // 所有的入参
 type Kv map[string]interface{}
 
 // 尝试获取字符串类型
-func (vars Kv) GetString(key string) (string, bool) {
+func (vars Kv) GetString(key string) (string, error) {
 	val, ok := vars[key].(string)
-	return val, ok
+	if !ok {
+		return val, fmt.Errorf("failed to get string by key(%s)", key)
+	}
+	return val, nil
 }
 
 // 尝试获取字符串类型，如果失败返回 defaultVal
@@ -19,14 +26,35 @@ func (vars Kv) GetStringOrDefault(key, defaultVal string) string {
 }
 
 // 尝试获取整数类型
-func (vars Kv) GetInt64(key string) (int64, bool) {
-	val, ok := vars[key].(int64)
-	return val, ok
+func (vars Kv) GetInt(key string) (int, error) {
+	val, ok := vars[key].(int)
+	if !ok {
+		return val, fmt.Errorf("failed to get int64 by key(%s)", key)
+	}
+	return val, nil
 }
 
 // 尝试获取整数类型，如果失败返回 defaultVal
-func (vars Kv) GetInt64OrDefault(key string, defaultVal int64) int64 {
-	val, ok := vars[key].(int64)
+func (vars Kv) GetInt64OrDefault(key string, defaultVal int) int {
+	val, ok := vars[key].(int)
+	if !ok {
+		return defaultVal
+	}
+	return val
+}
+
+// 尝试获取布尔类型
+func (vars Kv) GetBool(key string) (bool, error) {
+	val, ok := vars[key].(bool)
+	if !ok {
+		return val, fmt.Errorf("failed to get bool by key(%s)", key)
+	}
+	return val, nil
+}
+
+// 尝试获取布尔类型，如果失败返回 defaultVal
+func (vars Kv) GetBoolOrDefault(key string, defaultVal bool) bool {
+	val, ok := vars[key].(bool)
 	if !ok {
 		return defaultVal
 	}
