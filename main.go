6bb44ce97484e5a7698d0ef3532e2b5aa8f27651
WqVoon <2027195958@qq.com>
2023-02-09 16:48:33 +0800

    docs: 修改注释

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package be2fn
 import "github.com/wqvoon/be2fn/internal"
 
 // 传给编译出的函数的参数，key 是字符串，value 是 interface{}，
-// 目前 key 的类型在执行期根据二元表达式的另一个参数确定
+// 目前 key 的类型在编译期根据二元表达式的另一个参数确定
 type Kv = internal.Kv
 
 // 将 expr 编译为一个可执行的函数，编译失败时返回错误原因
